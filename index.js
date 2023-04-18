46a6ffa393832d59e0959386a015c3d5ef6e6b1e
samoteur13 <s.chabani13@gmail.com>
2023-04-18 14:57:37 +0200

    debu tuto react hello world et props

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import HelloWorldApp from './HelloWorld';
+import HelloProps from './HelloProps';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => HelloProps);
