d36eb17f2cec53b0d311316fd31b5d0f3a6e6c91
samoteur13 <s.chabani13@gmail.com>
2023-04-21 11:58:26 +0200

    dug

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+import Routes from './routes/Routes'
+
+
+
+
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+        </View>
+          <Text>cocuou</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
