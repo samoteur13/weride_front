d6771c2ad3fe94b9ad8e5d1dcd71a97a6ab28d0f
samoteur13 <s.chabani13@gmail.com>
2023-08-12 15:03:08 +0200

    modif headerConenct + add pages

---
+++ src/screens/events/EventCreateUpdate.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+export const EventCreateUpdate = () => {
+  return (
+    <Text style={{color: 'red', textAlign: 'center', fontSize: 20}}>
+      Ajouter ou modifier une sortie moto en construction
+    </Text>
+  );
+};
---
+++ src/screens/events/Events.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+export const Events = () => {
+  return (
+    <Text style={{color: 'red', textAlign: 'center', fontSize: 20}}>
+      Sorties moto en construction
+    </Text>
+  );
+};
